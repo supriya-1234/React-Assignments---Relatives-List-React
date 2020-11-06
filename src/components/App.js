fe2fcc5cbaaf5573658004885323513e997d6e36
supriya-1234 <66170913+supriya-1234@users.noreply.github.com>
2020-11-07 00:04:09 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import "../styles/App.css";
 
 class App extends Component {
   render() {
-    let relatives = ["Ruby", "Alok", "Akash", "Jyoti"];
+    const relatives = ["Ruby", "Alok", "Akash", "Jyoti"];
     return (
       <>
         <ol key="relativeList">
